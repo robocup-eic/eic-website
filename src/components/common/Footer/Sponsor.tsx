73355b95cffe7a3124c847b3e7455e98159e32f4
betich <betich365@gmail.com>
2022-11-15 17:59:33 +0700

    feat: update footer color based on pathname

--- src/components/common/Footer/Sponsor.tsx
+++ src/components/common/Footer/Sponsor.tsx
@@ -1,11 +1,20 @@
-export default function Sponsor() {
+import clsx from "clsx"
+
+interface SponsorProps {
+  theme: "creamy" | "tomato" | "none"
+}
+
+export default function Sponsor({ theme }: SponsorProps) {
   return (
     <div className="flex justify-center gap-2 my-8">
       {Array(8)
         .fill("")
         .map((_, i) => i)
         .map((e) => (
-          <div key={e} className="bg-tomato w-8 h-8" />
+          <div
+            key={e}
+            className={clsx("w-8 h-8", theme === "tomato" && "bg-creamy", theme === "creamy" && "bg-tomato")}
+          />
         ))}
     </div>
   )
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,11 +1,37 @@
+import clsx from "clsx"
+import { useRouter } from "next/router"
+import { useMemo } from "react"
 import Typography from "../Typography"
 import Sponsor from "./Sponsor"
 
 export default function Footer() {
+  const router = useRouter()
+  const theme = useMemo(() => {
+    const pathnames = router.pathname.split("/")
+    const route = pathnames[1]
+    if (route === "") return "creamy"
+    else if (route === "projects") {
+      if (pathnames.length > 2) return "tomato"
+      else return "creamy"
+    } else if (route === "contact") {
+      return "none"
+    } else return "tomato"
+  }, [router])
+
   return (
-    <footer className="text-center p-10">
-      <Sponsor />
-      <Typography className="font-bold text-xs">© 2022 EIC, Chulalongkorn University</Typography>
+    <footer
+      className={clsx(
+        "text-center p-10",
+        theme === "creamy" && "bg-creamy text-gray-900",
+        theme === "tomato" && "bg-tomato text-white"
+      )}
+    >
+      {theme !== "none" && (
+        <>
+          <Sponsor theme={theme} />
+          <Typography className="font-bold text-xs">© 2022 EIC, Chulalongkorn University</Typography>
+        </>
+      )}
     </footer>
   )
 }
