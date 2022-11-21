071f3f1ee7f3749c62b1666fd563cd229d3fe2da
betich <betich365@gmail.com>
2022-11-21 22:55:14 +0700

    feat: add page transition

--- src/components/common/PageLayout.tsx
+++ src/components/common/PageLayout.tsx
@@ -1,16 +1,34 @@
 import { ReactNode } from "react"
 import Navbar from "./Nav"
 import Footer from "./Footer"
+import { useRouter } from "next/router"
+import { motion } from "framer-motion"
 
 interface PageLayoutProps {
   children: ReactNode | ReactNode[]
 }
 
+const variants = {
+  initial: { y: -20, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+}
+
 export default function PageLayout({ children }: PageLayoutProps) {
+  const router = useRouter()
+
   return (
     <main className="relative bg-creamy text-gray-900 min-h-screen">
       <Navbar />
-      {children}
+      <motion.div initial="initial" animate="animate" variants={variants} key={router.pathname}>
+        {children}
+      </motion.div>
       <Footer />
     </main>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,21 @@ import "@/styles/tailwind.css"
 import "@/styles/fonts.css"
 import type { AppProps } from "next/app"
 import PageLayout from "@/components/common/PageLayout"
+import { AnimateSharedLayout } from "framer-motion"
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.addEventListener("touchstart", () => null, {
+      passive: true,
+    })
+  }, [])
+
   return (
     <PageLayout>
-      <Component {...pageProps} />
+      <AnimateSharedLayout>
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
     </PageLayout>
   )
 }
