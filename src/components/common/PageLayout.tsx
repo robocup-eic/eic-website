cd49451ee35462634a75b107206624c2d5b7892e
betich <betich365@gmail.com>
2022-11-23 09:33:59 +0700

    chore: remove locomotive scroll

--- src/components/common/PageLayout.tsx
+++ src/components/common/PageLayout.tsx
@@ -1,9 +1,10 @@
-import { ReactNode, useRef } from "react"
+// import { ReactNode, useRef } from "react"
+import { ReactNode } from "react"
 import Navbar from "./Nav"
 import Footer from "./Footer"
 import { useRouter } from "next/router"
 import { motion } from "framer-motion"
-import { LocomotiveScrollProvider } from "react-locomotive-scroll"
+// import { LocomotiveScrollProvider } from "react-locomotive-scroll"
 
 interface PageLayoutProps {
   children: ReactNode | ReactNode[]
@@ -23,31 +24,32 @@ const variants = {
 
 export default function PageLayout({ children }: PageLayoutProps) {
   const router = useRouter()
-  const containerRef = useRef<HTMLDivElement>(null)
+  // const containerRef = useRef<HTMLDivElement>(null)
 
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        lerp: 0.25,
-        // ... all available Locomotive Scroll instance options
-      }}
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      containerRef={containerRef}
-    >
-      <main data-scroll-container ref={containerRef} className="relative bg-creamy text-gray-900 min-h-screen">
-        <Navbar />
-        <motion.div initial="initial" animate="animate" variants={variants} key={router.pathname}>
-          {children}
-        </motion.div>
-        <Footer />
-      </main>
-    </LocomotiveScrollProvider>
+    // <LocomotiveScrollProvider
+    //   options={{
+    //     smooth: true,
+    //     lerp: 0.25,
+    //     // ... all available Locomotive Scroll instance options
+    //   }}
+    //   watch={
+    //     [
+    //       //..all the dependencies you want to watch to update the scroll.
+    //       //  Basicaly, you would want to watch page/location changes
+    //       //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+    //     ]
+    //   }
+    //   containerRef={containerRef}
+    // >
+    // <main data-scroll-container ref={containerRef} className="relative bg-creamy text-gray-900 min-h-screen">
+    <main className="relative bg-creamy text-gray-900 min-h-screen">
+      <Navbar />
+      <motion.div initial="initial" animate="animate" variants={variants} key={router.pathname}>
+        {children}
+      </motion.div>
+      <Footer />
+    </main>
+    // </LocomotiveScrollProvider>
   )
 }
