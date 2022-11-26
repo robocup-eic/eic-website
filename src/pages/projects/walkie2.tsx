c569e77a3df026cc1ea205ee15761d6b5cd96458
Game Limsila <limsila.limsila@yahoo.com>
2022-11-26 16:15:11 +0700

    I tired

--- src/pages/projects/walkie2.tsx
+++ src/pages/projects/walkie2.tsx
@@ -1,6 +1,7 @@
 import LinkButton from "@/components/common/LinkButton"
 import Typography from "@/components/common/Typography"
 import { DescribeRoute } from "@/components/Meta/DescribeRoute"
+import Image from "next/image"
 
 export default function Page() {
   return (
@@ -8,7 +9,7 @@ export default function Page() {
       {/* <section className=" relative mx-12 h-1/6 "> */}
       <section className="full-w-screen-2xl relative mx-12 h-1/6 ">
         <span className=" mx-auto  h-1/6 p-10  ">
-          <img src="/Walkie 2.svg" className="max-h-[60%] max-w-[60%] " />
+          <Image layout="fill" src="/Walkie 2.svg" className="max-h-[60%] max-w-[60%] " />
           <Typography type="h1" className="absolute top-0 right-0   text-right ">
             Walkie 2
           </Typography>
@@ -24,23 +25,23 @@ export default function Page() {
         </span>
         <section className="relative p-6">
           <Typography type="p" className="pt-10 indent-20">
-            An intelligent autonomous service robot, our EIC's friend, is built to participate in Robocup@home 2022
+            An intelligent autonomous service robot, our EIC&apos;s friend, is built to participate in Robocup@home 2022
             competition. We initiated Walkie in spring 2021 with a strong belief that Robots are good human companions.
             Walkie can do various household tasks and be a party host. Things he can do are picking and placing the
-            object on the shelf, tidying up the garbage on the floor, serving the meal up to the customer's
+            object on the shelf, tidying up the garbage on the floor, serving the meal up to the customer&apos;s
             order,helping hold a bag, and following a user.
           </Typography>
         </section>
 
         <span className="grid grid-flow-row-dense grid-cols-2 gap-20  p-10">
           <span className=" w-full">
-            <img className="w-full" src="/Rectangle 25.svg" alt="cant find shit" />
+            <Image layout="fill" className="w-full" src="/Rectangle 25.svg" alt="cant find shit" />
             <Typography type="caption" className="text-left">
               Walkie is a domestic service robot that helps you to do house chores and be your great companion.
             </Typography>
           </span>
           <span className=" w-full">
-            <img className="w-full" src="/Rectangle 25.svg" alt="cant find shit" />
+            <Image layout="fill" className="w-full" src="/Rectangle 25.svg" alt="cant find shit" />
             <Typography type="caption" className="text-left">
               We might think that our daily routine is boring and simple. But, for a robot like Walkie, a simple task
               may require much complex planning and motion.
@@ -48,14 +49,14 @@ export default function Page() {
           </span>
 
           <span className="  w-full">
-            <img className=" w-full" src="/Rectangle 25.svg" alt="cant find shit" />
+            <Image layout="fill" className=" w-full" src="/Rectangle 25.svg" alt="cant find shit" />
             <Typography type="caption" className="text-left">
               Walkie can arrange groceries in the cabinet, take out the trash, carry your bag, serve the cool drink from
               the refrigerator, and communicate with you in English.
             </Typography>
           </span>
           <span className=" w-full">
-            <img className=" w-full" src="/Rectangle 25.svg" alt="cant find shit" />
+            <Image layout="fill" className=" w-full" src="/Rectangle 25.svg" alt="cant find shit" />
             <Typography type="caption" className="text-left">
               Artificial Intelligence plays a significant role in making Walkie reliable and making intelligent
               decisions in doing boring human routines.
@@ -64,8 +65,8 @@ export default function Page() {
         </span>
         <section className=" p-6">
           <Typography className="text-center" type="caption">
-            Our team, EIC from Chulalongkorn University, is developing Walkie's body and brain to make our robot not
-            just a machine. He is our friend.
+            Our team, EIC from Chulalongkorn University, is developing Walkie&apos;s body and brain to make our robot
+            not just a machine. He is our friend.
           </Typography>
           <span className="my-12  flex items-center justify-center ">
             <LinkButton
