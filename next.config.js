92b881507fcf1b73b17e4d8812651e9e64a065c1
betich <betich365@gmail.com>
2022-12-11 20:34:32 +0700

    ci: revert settings

--- next.config.js
+++ next.config.js
@@ -15,8 +15,8 @@ module.exports = withPWA({
     domains: ["via.placeholder.com"],
     unoptimized: true,
   },
-  // assetPrefix: process.env.NODE_ENV === "production" ? "/eic-website/" : "/",
-  // basePath: process.env.NODE_ENV === "production" ? "/eic-website" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/eic-website/" : "/",
+  basePath: process.env.NODE_ENV === "production" ? "/eic-website" : "",
   webpack(config, options) {
     config.resolve.alias = {
       ...config.resolve.alias,
